bfe0d45201f3e545d2a1cd405d96385e1dfa8419
Paulo-Mansano <145067044+Paulo-Mansano@users.noreply.github.com>
2023-11-27 15:20:51 -0300

    Update Window.java

--- Window.java
+++ Window.java
@@ -2,8 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Window extends JFrame {
+    Main main = new Main();
     public Window() {
         setTitle("Sistema de Gestão Escolar");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -116,7 +120,7 @@ public class Window extends JFrame {
                 } else if (botaoClicado.getText().equals("ACESSAR DADOS ALUNO")) {
                     // new JAnelaDadosAlunos();
                 } else if (botaoClicado.getText().equals("VISUALIZAR ALUNOS")) {
-                    new JanelaVisualizarAlunos();
+                    new JanelaVisualizarAlunos(main);
                 } else if (botaoClicado.getText().equals("EDITAR ALUNO")) {
                     new JanelaEditarAluno();
                 }
@@ -163,13 +167,13 @@ public class Window extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 JButton botaoClicado = (JButton) e.getSource();
                 if (botaoClicado.getText().equals("CADASTRO DE PROFESSORES")) {
-                    
+                    new JanelaCadastroProfessor();
                 } else if (botaoClicado.getText().equals("ALOCAÇÃO DE PROFESSORES")) {
-                   
+    
                 } else if (botaoClicado.getText().equals("REALOCAÇÃO DE PROFESSORES")) {
                   
                 } else if (botaoClicado.getText().equals("VISUALIZAR PROFESSORES")) {
-                   
+                   new JanelaVisualizarProfessores();
                 } else if (botaoClicado.getText().equals("ACESSAR DADOS DE PROFESSORES")) {
                     
                 } else if (botaoClicado.getText().equals("EDITAR PROFESSORES")) {
@@ -295,7 +299,225 @@ public class Window extends JFrame {
 
     }
 
-    class JanelaVisualizarAlunos extends JFrame {
+    public class JanelaVisualizarAlunos extends JFrame {
+        private Map<Integer, Aluno> alunos;
 
+        public JanelaVisualizarAlunos(Main main) {
+            setTitle("Visualizar Alunos");
+            setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            setSize(400, 300);
+            setLocationRelativeTo(null);
+
+            this.alunos = main.alunos; // Acessa o mapa 'alunos' da instância de Main
+
+            // Chame o método para criar e exibir a lista de alunos
+            exibirListaAlunos();
+
+            setVisible(true);
+        }
+
+        private void exibirListaAlunos() {
+            // Extrai os nomes dos alunos como um array de Strings
+            String[] nomesAlunos = alunos.values().stream().map(Aluno::getNome).toArray(String[]::new);
+
+            // Cria a JList com os nomes dos alunos
+            JList<String> listaAlunos = new JList<>(nomesAlunos);
+
+            // Adiciona a JList a um JScrollPane para permitir rolar
+            JScrollPane scrollPane = new JScrollPane(listaAlunos);
+            add(scrollPane);
+        }
+    }
+    public class JanelaCadastroProfessor extends JFrame {
+    private JTextField campoNome;
+    private JTextField campoCPF;
+    private JTextField campoMatricula;
+    private JTextField campoEmail;
+    private JTextField campoEmailAcademico;
+
+    public JanelaCadastroProfessor() {
+        setTitle("Cadastro de Professor");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(7, 2));
+
+        panel.add(new JLabel("NOME: (Obrigatório)"));
+        campoNome = new JTextField();
+        panel.add(campoNome);
+
+        panel.add(new JLabel("CPF (Obrigatório):"));
+        campoCPF = new JTextField();
+        panel.add(campoCPF);
+
+        panel.add(new JLabel("MATRICULA (Opcional - 3 DÍGITOS):"));
+        campoMatricula = new JTextField();
+        panel.add(campoMatricula);
+
+        panel.add(new JLabel("EMAIL (Obrigatório):"));
+        campoEmail = new JTextField();
+        panel.add(campoEmail);
+
+        panel.add(new JLabel("EMAIL ACADÊMICO (Opcional):"));
+        campoEmailAcademico = new JTextField();
+        panel.add(campoEmailAcademico);
+
+        JButton btnCadastrar = new JButton("Cadastrar");
+        btnCadastrar.addActionListener(new BotaoCadastrarListener());
+        panel.add(btnCadastrar);
+
+        add(panel);
+        setVisible(true);
+    }
+
+    private class BotaoCadastrarListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                cadastrarProfessor();
+            } catch (IllegalArgumentException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private void cadastrarProfessor() {
+        System.out.println("INICIANDO CADASTRO DE PROFESSOR...");
+
+        String nomeCompleto = campoNome.getText().trim();
+        if (nomeCompleto.isEmpty()) {
+            throw new IllegalArgumentException("Nome não pode ser nulo.");
+        }
+
+        String cpf = campoCPF.getText().replaceAll("[.\\-\\s]", "");
+        if (cpf.isEmpty()) {
+            throw new IllegalArgumentException("CPF não pode ser nulo.");
+        }
+
+        Integer matricula = null;
+        String matriculaInput = campoMatricula.getText().trim();
+        if (!matriculaInput.isEmpty()) {
+            matricula = Integer.parseInt(matriculaInput);
+        }
+
+        String email = campoEmail.getText().trim();
+        if (email.isEmpty()) {
+            throw new IllegalArgumentException("Email não pode ser nulo.");
+        }
+
+        String emailAcad = campoEmailAcademico.getText().trim();
+
+        
+        System.out.println("\nDISCIPLINAS DISPONÍVEIS: ");
+
+        dispose();
+    }
+  }
+
+  public class JanelaAlocacaoProfessor extends JFrame {
+
+    private JComboBox<String> comboProfessores;
+    private JComboBox<String> comboTurmas;
+
+    public JanelaAlocacaoProfessor(Map<Integer, Professor> professores, Map<Integer, Turma> turmas) {
+        setTitle("Alocação de Professor");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 2));
+
+        // Criar listas de nomes de professores e turmas
+        String[] nomesProfessores = new String[professores.size()];
+        String[] nomesTurmas = new String[turmas.size()];
+
+        int i = 0;
+        for (Entry<Integer, Professor> entry : professores.entrySet()) {
+            nomesProfessores[i] = entry.getKey() + ": " + entry.getValue().getNome();
+            i++;
+        }
+
+        i = 0;
+        for (Entry<Integer, Turma> entry : turmas.entrySet()) {
+            nomesTurmas[i] = entry.getKey() + ": " + entry.getValue().getId();
+            i++;
+        }
+
+        comboProfessores = new JComboBox<>(nomesProfessores);
+        comboTurmas = new JComboBox<>(nomesTurmas);
+
+        panel.add(new JLabel("Professores:"));
+        panel.add(comboProfessores);
+
+        panel.add(new JLabel("Turmas:"));
+        panel.add(comboTurmas);
+
+        JButton btnAlocar = new JButton("Alocar Professor");
+        btnAlocar.addActionListener(new BotaoAlocarListener(professores, turmas));
+        panel.add(btnAlocar);
+
+        add(panel);
+        setVisible(true);
+    }
+
+    private class BotaoAlocarListener implements ActionListener {
+        private final Map<Integer, Professor> professores;
+        private final Map<Integer, Turma> turmas;
+
+        public BotaoAlocarListener(Map<Integer, Professor> professores, Map<Integer, Turma> turmas) {
+            this.professores = professores;
+            this.turmas = turmas;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int idProfessorSelecionado = Integer.parseInt(
+                    comboProfessores.getSelectedItem().toString().split(":")[0].trim()
+            );
+            int idTurmaSelecionada = Integer.parseInt(
+                    comboTurmas.getSelectedItem().toString().split(":")[0].trim()
+            );
+
+            // Verificar se o ID selecionado corresponde a um professor existente
+            if (professores.containsKey(idProfessorSelecionado)) {
+                Professor professorSelecionado = professores.get(idProfessorSelecionado);
+
+                // Verificar se o ID selecionado corresponde a uma turma existente
+                if (turmas.containsKey(idTurmaSelecionada)) {
+                    Turma turmaSelecionada = turmas.get(idTurmaSelecionada);
+
+                    // Alocar professor na turma selecionada
+                    turmaSelecionada.setProfessor(professorSelecionado);
+                    professorSelecionado.setTurmas(turmaSelecionada);
+
+                    System.out.println("Professor alocado na turma com sucesso.");
+                } else {
+                    System.out.println("ID de turma inválido. Tente novamente.");
+                }
+            } else {
+                System.out.println("ID de professor inválido. Tente novamente.");
+            }
+            
+
+            // Fechar a janela após a alocação
+            dispose();
+            }
+        }
+    }
+
+        public class JanelaVisualizarProfessores extends JFrame {
+
+            public JanelaVisualizarProfessores() {
+                setTitle("Visualizar Professores Cadastrados");
+                setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                setSize(400, 300);
+                setLocationRelativeTo(null);
+        
+                JPanel panel = new JPanel();
+        
+        }
     }
 }
